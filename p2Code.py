edeb584591f45e28997494f4448afc68520d48b1
Geoffrey Llopis <llopis.geoffrey@laposte.net>
2021-05-21 05:37:40 +0200

    Utilisation d'un dictionnaire pour stocker information. Données récupérer : upc ; prix taxé ; prix sans taxe ; quantité. Pour un livre

--- p2Code.py
+++ p2Code.py
@@ -10,16 +10,16 @@ enTete = ['product_page_url', 'universal_product_code (upc)', 'title']
 
 def ecritureCSV(infosAEcrire):
     with open('leFichier.csv', 'w') as file:
-        file.write('prdouct_page_url|universal_product_code (upc)|title|price_including_tax|'
+        file.write('product_page_url|universal_product_code (upc)|title|price_including_tax|'
                    'price_excluding_tax|number_available|product_description|category|'
                    'review_rating|image_url' + '\n\n')
-        for element in infosAEcrire:
-            if infosAEcrire[0] == infosAEcrire[0]:
-                file.write(element + '|')
-            elif infosAEcrire[2] == infosAEcrire[2]:
-                file.write(element in file[2])
+        print('dictionnaire', infosAEcrire)
+        for cle,information in infosAEcrire.items():
+            if information == '':
+                file.write('none' + '|')
             else:
-                continue
+               file.write(information + '|')
+        file.write('\n')
 
 
 
@@ -29,57 +29,43 @@ def lectureCSV(infosALire):
             print('row', row)
 
 
-def scrapProductInformation():
+def scrapInformationVoulues():
     if response.ok:
-        pI = []
-        for i in range(10):
-            pI.append('none')
+        informationVoulues = {
+            'product_page_url': '',
+            'universal_product_code (upc)': '',
+            'title': '',
+            'price_including_tax': '',
+            'price_excluding_tax': '',
+            'number_available': '',
+            'product_description': '',
+            'category': '',
+            'review_rating': '',
+            'image_url': ''
+        }
         soup = BeautifulSoup(response.text, 'html.parser')
         tds = soup.findAll('td')
         for i in range(len(tds)):
             if i == 0:
-                #productInformation = tds[i].text
-                pI[i] = tds[i].text
+                informationVoulues['universal_product_code (upc)'] = tds[i].text
             if i == 2 or i == 3:
-               # productInformation = tds[i].text.replace('.', ',').replace('Â', '').replace('\u00A3', '\u00A3 ')
-                pI[i] = tds[i].text.replace('.', ',').replace('Â', '').replace('\u00A3', '\u00A3 ')
+                informationVoulues['price_excluding_tax'] = tds[i].text.replace('.', ',').replace('Â', '').replace('\u00A3', '\u00A3 ')
+            if i == 3:
+                informationVoulues['price_including_tax'] = tds[i].text.replace('.', ',').replace('Â', '').replace('\u00A3', '\u00A3 ')
             elif i == 5:
                 if str(tds[i].text[0]) == str('I'):
-                    pI[i] = tds[i].text[10:-11]
+                    informationVoulues['number_available'] = tds[i].text[10:-11]
                 else:
-                    pI[i] = tds[i].txt[11:-11]
+                    informationVoulues['number_available'] = tds[i].txt[11:-11]
             else:
                 continue
-        print('tds', tds)
-        print('pI', pI)
-        ecritureCSV(pI)
+        ecritureCSV(informationVoulues)
 
 
-"""
-repartition = {0: '',: .text.replace('.', ',').replace('Â', '').replace('\u00A3', '\u00A3 '),
-                    3
-                    5In: .text[10:-11], 5Out: .txt[11:-11], entree: tds[i].text, sortie: pI[i]}
-    for i in range(len(tds)):
-        repartition.get(-1) = repartition.get(-2) + repartition.get(i)
-            if i == 0:
-                repartition.key[-1] = repartition.get(-2)
-"""
 
-scrapProductInformation()
+scrapInformationVoulues()
 lectureCSV('leFichier')
 
-""""
-def test():
-    list = ['a', 'b', 'a', 'd']
-    for i in range(len(list)):
-        if list[0] == list[0]:
-            list[1] = str('e')
-        else:
-            continue
-    print(list)
-
-test()
-"""
 
 
 """ IDÉE
