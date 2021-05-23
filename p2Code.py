54c400cb409456ef7ec4cff3ac137330027476b9
Geoffrey Llopis <llopis.geoffrey@laposte.net>
2021-05-23 03:19:24 +0200

    Speudo PEP-8 + fonction controle_url_books (vérifie avant scrap, que les url des home page des livres renvoi un code 200, sinon message d'erreur) + nom du fichier d'écriture selon le nom de la categorie scrapper (data_books_in_categorieScrapper_category.csv)

--- p2Code.py
+++ p2Code.py
@@ -2,21 +2,22 @@ import requests as rq
 from bs4 import BeautifulSoup
 # import time
 
-# urlLivre = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+
+# Fonctions à ajouter.
+
+### Fonction pour vérifier que toutes les clés ont bien la même quantité de
+# valeur
+# Fonction à effectuer avant l'écriture du .csv Afin d'éviter des décalages dans
+# les informations.
+# Sinon exemple la description de exemple_book pourrait se retrouvée avec les
+# autres infos de other_book.
 #
-# response = rq.get(urlLivre)
-
-enTete = ['product_page_url', 'universal_product_code (upc)', 'title']
-
-
-##### Deux fonction a ajouter
-### Fonction pour vérifier que toutes les clés est bien la même quantité de valeur ###
-    # Foncion a effectuer avant l'écriture du .csv
-    # Afin d'éviter des décalages dans les informations.
-    # Sinon exemple la description de exempleLivre pourrait se retrouver avec les autres infos de autreLivre
+# Première ébauche de la fonction………
 # def fonctionControleQuantiteParCle():
-#     # Avant d'écrire le fchier on vérifier que chaque clée a bien la même qunatité de valeurs
-#     # Si oui on écrire le .csv si non on renvoi un message d'erreur en présissant  ou est l'erreur
+#     # Avant d'écrire le fchier on vérifier que chaque clée a bien la même
+# quantité de valeurs
+#     # Si oui on écrire le .csv, sinon on renvoi un message d'erreur en
+# présissant où est l'erreur.
 #     quantiteValeurParCle = []
 #     erreurQuantiteValeur = []
 #     for cleDicoInformationsVoulues in infosAEcrire.keys():
@@ -28,124 +29,178 @@ enTete = ['product_page_url', 'universal_product_code (upc)', 'title']
 #     else:
 #         print('Toutes les clés n\'ont pas la même quantité de valeurs\n')
 #         for i in range(len(infosAEcrire.keys()):
-#             print('%s \t: %d' % (infosAEcrire.keys(i), quantiteValeurParCle[i]))
+#           print('%s \t: %d' % (infosAEcrire.keys(i), quantiteValeurParCle[i]))
 
-### Fonction pour connaître la nature de l'url ###
-    # S'il s'agit de l'url d'une category, exécuter la fonction toutesURLCategory
-    # S'il sagit de l'url d'un livre, exécuter directement la foncion scrapInformationsVoulus
+### Fonction pour connaître la nature de l'url
 
+# S'il s'agit de l'url d'une categorie, exécuter la
+# fonction collect_url_books_by_one_category().
+# S'il sagit de l'url d'un livre, exécuter directement la foncion scrap_data()
+
+### Fonction envisagées.
 # Mettre toutes les fonctions dans une fonction les réunissant toutes ???
-# Ou plutôt une foncion qui puisse toutes les appeler lorsqu'il le faut
+# Ou plutôt une foncion qui puisse toutes les appeler lorsqu'il le faut ???
 
 
+# Écriture dans un .csv des informations récoltées de chaque url visités.
+def write_data_desired_in_csv(data_desired):
+    name_file_data_books = 'data_books_in_'\
+                           + data_desired.get('category')[0]\
+                           + '_category'\
+                           + '.csv'
+    quantity_values_by_key = len(data_desired['title'])
+    # Même si toutes les valeurs du dictionnaire data_desired{} sont censé être
+    # en utf-8, il faut quant même encoder le .csv en utf-8, car j'ai eu une
+    # erreur pour la catégorie christian à cause d'un espace insécable dans la
+    # description du livre "Blue Like[…]", présent juste après
+    # "[_]passion in[_]". L'espace insécable est afficher par "\xà0", ce qui
+    # n'est pas du utf-8.
+    # Préciser encodaging='utf-8' paramètre du .csv m'a levée cette erreur.
+    with open(name_file_data_books, 'w', encoding='utf-8') as file_data_books:
+        # Le noms des clés de data_desired servent comme nom de colonnes.
+        for key_data_desired in data_desired.keys():
+            file_data_books.write(key_data_desired + '|')
+        file_data_books.write('\n\n')
 
-### Fonction pour écrire dans un .csv les infortions récoltées ###
-def ecritureCSV(infosAEcrire):
-    quantiteValeurParCle = len(infosAEcrire['title'])
-# Même si toutes les valeurs du dictionnaire informationsVoulues{} sont censé être en utf-8
-# il faut quant même encoder le .csv en utf-8 car j'ai eu une erreur pour la catégorie christian à cause d'un espace
-# insécable (\xà0) qui n'était pas utf-8.
-# Présicer encodage du .csv m'a levée cette erreur.
-    with open('leFichier.csv', 'w', encoding='utf-8') as file:
-        # En-tête du fichier. Sert comme nom de colonnes
-        file.write('product_page_url|universal_product_code (upc)|title|price_including_tax|'
-                   'price_excluding_tax|number_available|product_description|category|'
-                   'review_rating|image_url' + '\n\n')
-    # Écriture des informations
-    # On écrit la iémeValeur de chaque clé sur un ligne puis au fais un saut de ligne
-        for iemeValeur in range(quantiteValeurParCle):
-            for cleDicoInformationsVoulues in infosAEcrire.keys():
-                file.write(infosAEcrire.get(cleDicoInformationsVoulues)[iemeValeur] + '|')
-            if iemeValeur < quantiteValeurParCle - 1:
-                file.write('\n')
-
-### Fonction pour lire le .csv contenant les informations récoltées ###
-def lectureCSV(infosALire):
-    with open(infosALire + '.csv', 'r') as file:
-       for row in file:
-           print('row', row)
+    # Écriture des informations.
+    # On écrit la iémeValeur de chaque clé sur un ligne, puis un saut deligne.
+        for ieme_values in range(quantity_values_by_key):
+            for key_data_desired in data_desired.keys():
+                file_data_books\
+                    .write(data_desired.get(key_data_desired)[ieme_values])
+                if key_data_desired != 'image_url':
+                    file_data_books.write('|')
+            if ieme_values < quantity_values_by_key - 1:
+                file_data_books.write('\n')
 
 
-### Fonction pour récupérer toutes les informations voulues sur l'url d'un livre ###
-def scrap_data(urlLivre):
-    informationVoulues = { 'product_page_url': [],
-                            'universal_product_code (upc)': [],
-                            'title': [],
-                            'price_including_tax': [],
-                            'price_excluding_tax': [],
-                            'number_available': [],
-                            'product_description': [],
-                            'category': [],
-                            'review_rating': [],
-                            'image_url': []}
+# Fonction pour lire le .csv contenant les informations récoltées.
+def read_csv_content_data_desired(nam_file_data_books):
+    with open(nam_file_data_books, 'r') as file_data_books:
+        for row in file_data_books:
+            print('row', row)
 
-    for urlATraiter in urlLivre:
-        response_urlLivres = rq.get(urlATraiter)
 
-        if response_urlLivres.ok:
+# Collecte de toutes les informations pour chaque url contenues dans la liste
+# url_books
+def scrap_data(url_books):
+    data_desired = {'product_page_url': [],
+                    'universal_product_code (upc)': [],
+                    'title': [],
+                    'price_including_tax': [],
+                    'price_excluding_tax': [],
+                    'number_available': [],
+                    'product_description': [],
+                    'category': [],
+                    'review_rating': [],
+                    'image_url': []}
 
-        # L'ensemble des soups
-            # Nécessaire de préciser a BeautifulSoup que le contenu de respons_urlLivre doit être lu en tant que utf-8
-            # Cela permet la compréhension de tous les caractères (utile pour caractères particulier présent en description)
-            # En revanche cela marche uniquement parce que la page à été encoder en utf-8
-            # Cela implique que soupLivreHome est transformé en str() donc la méthode .text() ne doit pas être appliquée
-            soupLivreHome = BeautifulSoup(response_urlLivres.content.decode('utf-8'), 'html.parser')
-            tdsTagProductInformation = soupLivreHome.findAll('td')
-            h1StrTitreOeuvre = soupLivreHome.find('h1').text
-            pTagDescriptionEtNote = soupLivreHome.findAll('p')
-            aStrCategoryOeuvre = soupLivreHome.findAll('a')[3].text
-            img_urlImage = soupLivreHome.findAll('img')[0].attrs.get('src')
+    for url_book in url_books:
+        request_url_book = rq.get(url_book)
+        # Nécessaire de préciser à BeautifulSoup que le contenu de
+        # request_url_book doit être lu en tant que utf-8.
+        # Cela permet la compréhension de tout les caractères (utile pour
+        # caractères particuliers présent dans de nombreuses descriptions).
+        # Cela implique que soup_book_home est transformée en str() donc la
+        # méthode .text() ne doit pas être appliquée.
+        soup_book_home = BeautifulSoup(request_url_book.content
+                                       .decode('utf-8'), 'html.parser')
+        tdstag_content_product_information = soup_book_home.findAll('td')
+        h1_title_book = soup_book_home.find('h1').text
+        ptag_description_and_rating = soup_book_home.findAll('p')
+        a_category_book = soup_book_home.findAll('a')[3].text
+        img_url_cover_book = soup_book_home.findAll('img')[0].attrs.get('src')
 
-        # Ajout de l'url de la page
-            informationVoulues['product_page_url'].append(urlATraiter)
-        # Ajout des informations issues de balises <td>
-            informationVoulues['universal_product_code (upc)'].append(tdsTagProductInformation[0].text)
-            # Le caractère unicode "\u00A3" est celui de la livre Sterling on ajoute un espace après celui-ci
-            # Remplacement des séparateur décimaux "." en ","
-            informationVoulues['price_excluding_tax'].append(
-                    tdsTagProductInformation[2].text.replace('.', ',').replace('Â', '').replace('\u00A3', '\u00A3 '))
-            informationVoulues['price_including_tax'].append(
-                    tdsTagProductInformation[3].text.replace('.', ',').replace('Â', '').replace('\u00A3', '\u00A3 '))
-            # Extraction de la quantié disponible dans "In stock (xx available)"
-            # Supposition que s'il n'y en a pas en stock alors le texte ne débutera pas par "I"
-            # S'il débute par "I" alors on récupére la quantité en stock sinon on indique 0
-            if tdsTagProductInformation[5]:
-                if str(tdsTagProductInformation[5].text[0]) == str('I'):
-                    informationVoulues['number_available'].append(tdsTagProductInformation[5].text[10:-11])
-                else:
-                    informationVoulues['number_available'].append(0)
-        # Ajout de l'information issues de la balise <h1>
-            informationVoulues['title'].append(h1StrTitreOeuvre)
-        # Ajout des informationss issues de balises <p>
-            # On a un bon affichage des caractères particuliers (tel "…" "'")
-            # parce que soupLivreHome est decoder en utf-8
-            # informationVoulues['product_description'].append(DU UTF-8)
-            informationVoulues['product_description'].append(pTagDescriptionEtNote[3].text)
-            # Dictionnaire pour traduire en français le nombre d'étoiles qu'a un livre
-            # Information de forme "xx étoile(s)"
-            traduction = {'Zero': 'Zéro étoile',
-                          'One': 'Une étoile',
-                          'Two': 'Deux étoiles',
-                          'Three': 'Trois étoiles',
-                          'Four': 'Quatre étoiles',
-                          'Five': 'Cinq étoiles'}
-            informationVoulues['review_rating'].append(str(traduction.get(str(pTagDescriptionEtNote[2].attrs.get('class')[1]))))
-        # Ajout de l'information issue d'une balise <a>
-            informationVoulues['category'].append(aStrCategoryOeuvre)
-        # Ajout de l'information issues d'une balise <img>
-        # Il faut reconstruire l'url de l'image
-            informationVoulues['image_url'].append(str('https://books.toscrape.com/' + str(img_urlImage[6:])))
+        # Ajout des différentes informations dans le dictionnaire data_desired.
+        data_desired['product_page_url'].append(url_book)
+        data_desired['universal_product_code (upc)']\
+            .append(tdstag_content_product_information[0].text)
+        # Ajout d'un espace après le symbole de la livre Sterling (\u00A3)
+        # Remplacement du séparateur décimaux "." en ",".
+        data_desired['price_excluding_tax']\
+            .append(tdstag_content_product_information[2].text
+                    .replace('.', ',')
+                    .replace('\u00A3', '\u00A3 '))
+        data_desired['price_including_tax']\
+            .append(tdstag_content_product_information[3].text
+                    .replace('.', ',')
+                    .replace('\u00A3', '\u00A3 '))
+        # Collecte de la quantité d'unité en stock "In stock (xx available)"
+        # Supposition que s'il n'y en a pas en stock, alors le texte ne débutera
+        # pas par "I".
+        # S'il débute par "I", on récupére la quantité en stock, sinon on
+        # indique 0.
+        if tdstag_content_product_information[5]:
+            if str(tdstag_content_product_information[5].text[0]) == str('I'):
+                data_desired['number_available']\
+                    .append(tdstag_content_product_information[5].text[10:-11])
+            else:
+                data_desired['number_available'].append(0)
+        data_desired['title'].append(h1_title_book)
+        # Les caractères particuliers (tel "…" "'"), s'affiche correctement
+        # parce que soup_book_home est decoder en utf-8.
+        # data_desired['product_description'].append(est en utf-8)
+        data_desired['product_description']\
+            .append(ptag_description_and_rating[3].text)
+        # Dictionnaire pour traduire en français le nombre d'étoiles qu'a un
+        #  livre. data_desired['review_rating'] = "xx étoile(s)"
+        traduction = {'Zero': 'Zéro étoile',
+                      'One': 'Une étoile',
+                      'Two': 'Deux étoiles',
+                      'Three': 'Trois étoiles',
+                      'Four': 'Quatre étoiles',
+                      'Five': 'Cinq étoiles'}
+        data_desired['review_rating']\
+            .append(str(traduction
+                        .get(str(ptag_description_and_rating[2].attrs
+                                 .get('class')[1]))))
+        data_desired['category'].append(a_category_book)
+        # Il faut reconstruire l'url de l'image de la couverture du livre.
+        data_desired['image_url']\
+            .append(
+            str('https://books.toscrape.com/' + str(img_url_cover_book[6:])))
     # time.sleep()
-    ecritureCSV(informationVoulues)
+    write_data_desired_in_csv(data_desired)
 
 #  lectureCSV('leFichier')
+
+
+# Vérfie que les requêtes sur les url_books renvoi un code 200, puis appel la
+# fonction scrap_data(). Le cas échéant renvoi un message indiquant les
+# url_books invalides.
+def controle_url_books(url_books):
+    # Less 4 lignes en commentaires suivantse sont là pour introduires des
+    # erreurs dans des url_books et vérifier l'efficacité de la fonccion.
+    # url_books.insert(0, url_books[0] + 'l')
+    # url_books.pop(1)
+    # url_books.insert(1, url_books[1] + 'l')
+    # url_books.pop(2)
+    quantity_error_url_book = []
+    for url_book in url_books:
+        request_url_book = rq.get(url_book)
+        if request_url_book.ok is False:
+            quantity_error_url_book.append(url_books.index(url_book))
+        else:
+            continue
+    if len(quantity_error_url_book) == 0:
+        scrap_data(url_books)
+    else:
+        if len(quantity_error_url_book) == 1:
+            print('quantity_error_url_book', len(quantity_error_url_book))
+            print('\nL\'url :\n{0}\nest invalide !'
+                  .format(url_books[quantity_error_url_book[0]]))
+        else:
+            print('\nLes url :')
+            for index_url_books_error in quantity_error_url_book:
+                print(url_books[index_url_books_error])
+            print('sont invalides !')
 
 
 # Collecte de toutes les urls de tous les livres d'une categorie.
 def collect_url_books_by_one_category():
     url_category_home = '' \
                         'http://books.toscrape.com/catalogue/category/books/' \
-                        'christian_43/index.html'
+                        'contemporary_38/index.html'
 
     # Pour contrôle que l'url est valide. À terme déplacer CECI dans une
     # fonction qui le vérifie avant la collecte des url.
