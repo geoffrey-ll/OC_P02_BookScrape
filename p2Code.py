722b619f1434d37b23e02c46f2bdc1953e0b311d
Geoffrey Llopis <llopis.geoffrey@laposte.net>
2021-05-25 13:38:25 +0200

    Suppresion de la variable speudo-globale input_possible. \n    Speudo-PEP-8 fait : \n    [collect_category_possible ; demand_of_user_what_category_to_scrap] \n    Speudo-PEP-8 : \n    [analyze_and_collect_input_user] \n    Apport à analyze_and_collect_input_user : lors de l'affichage de la liste des catégories disponible pour le scrap, le nombre de lignes qui s'affichue s'adapte aux nombres de catégories

--- p2Code.py
+++ p2Code.py
@@ -305,85 +305,146 @@ def collect_url_home_category():
 
 
 
-
-
+#### Si dans l'input manul, le premier caractère n'est pas '-', alors rien ne se passe.
 # Identifie les catégories disponibles à scrapper, d'après celles récoltés dans
 # la fonction collect_url_home_category(). Puis laisse l'utilisateur définir les
 # catégories qui sont à scrapper.
 def selection_category_for_scrap(all_url_home_page_category_book):
-    input_possible = ['-help', '-all', '-list']
     category_possible = []
-    category_select_by_user = []
-    for category_book in all_url_home_page_category_book:
-        input_possible.append(category_book[51].upper() + category_book[52:-13].replace('_', '').replace('-', ' '))
-        category_possible.append(category_book[51].upper() + category_book[52:-13].replace('_', '').replace('-', ' '))
 
+    # Collecte les différents noms des catégories du site marchand et les ajoute
+    # à la liste nommée category_possible.
+    def collect_category_possible(all_url_home_page_category_book):
+        for category_book in all_url_home_page_category_book:
+            category_possible.append(category_book[51:-13]
+                                     .replace('_', '')
+                                     .replace('-', ' ')
+                                     .capitalize())
+        demand_of_user_what_category_to_scrap()
 
-
+    # Input pour connaître les catégories de livers à scrapper, avec
+    # présentations des différentes options possibles.
     # '-quit' plutôt qui '-q' ou 'q' pour éviter les sorties du script
     # accidentelles.
     def demand_of_user_what_category_to_scrap():
-        all_input_user = [input(('{0}'*56 + '\n#{1:^54}#\n'
+        all_input_user = [input(('{0}'*56
+                                 + '\n#{1:^54}#\n'
                                  + '{0}'*56 + '\n\n' +
                                  '{2:<16} : {3}\n' +
                                  '{4:<16} : {5}\n' +
                                  '{6:<16} : {7}\n' +
-                                 '{8:<16} : {9}\n').format('#',
-                                                    'Quelles catégories de livres sont à scraper ?',
-                                                    '-noms categories', 'toutes les entrées sur la même ligne',
-                                                    '-all', 'TOUTES les catégories',
-                                                    '-list', 'catégories disponible au scrap',
-                                                    '-quit', 'interrompt le script'))]
+                                 '{8:<16} : {9}\n')
+                                .format('#',
+                                        'Quelles catégories de livres sont à '
+                                        'scraper ?',
+                                        '-noms categories', 'toutes les entrées'
+                                                            ' sur la même ligne'
+                                                            '',
+                                        '-all', 'TOUTES les catégories',
+                                        '-list', 'catégories disponible au '
+                                                 'scrap',
+                                        '-quit', 'interrompt le script'))]
         analyze_and_collect_input_user(all_input_user)
 
-
+    # Exécute les options par défaut is présent dans l'input (-list or -quit or
+    # -all). Sinon envoi l'input dans la fonction suivant qui déterminera les
+    # catégories renseignées.
     def analyze_and_collect_input_user(all_input_user):
         for input_user in all_input_user:
-            if input_user == ('-list'):
-                category_possible_sorted = sorted(input_possible[3:])
-                print('\n{1:#^100}\n{2:<1s}{0:^98s}{2:>1s}\n{1:#^100}\n'.format('Catégories disponibles pour le scrapping.', '', '#'))
-                for start_index_category_show_by_row in range(0, len(category_possible_sorted), 5):
-                    end_index_show_by_row = start_index_category_show_by_row + 4
-                    print('{0:<20s}{1:<20s}{2:<20s}{3:<20s}{4:<20s}'.format(category_possible_sorted[start_index_category_show_by_row],
-                                                                            category_possible_sorted[start_index_category_show_by_row + 1],
-                                                                            category_possible_sorted[start_index_category_show_by_row + 2],
-                                                                            category_possible_sorted[start_index_category_show_by_row + 3],
-                                                                            category_possible_sorted[end_index_show_by_row]))
-                all_input_user = []
-                all_input_user.append(input(('\n'
-                                             + '{0:<16} : {1}\n' +
-                                             '{2:<16} : {3}\n' +
-                                             '{4:<16} : {5}\n')
-                                             .format('-noms catégories', 'toutes les entrées sur la même ligne',
-                                                     '-all', 'TOUTES les catégories',
-                                                     '-quit', 'interrompt le scrit')))
-
+            if input_user == '-list':
+                category_possible_sorted = sorted(category_possible)
+                # Titre de la liste de catégories.
+                print('\n{0:#^100}\n#{1:^98}#\n{0:#^100}\n'
+                      .format('',
+                              'Catégories disponibles pour le scrapping.'))
+                # Affichage des catégories de livres disponibles, rangées par
+                # ordre alphabétique et disposées en 5 colonnes (20 caractères
+                # par nom de catégories, alignement gauche) sur le nombres de
+                # lignes nécessaires.
+                number_row_less_one = len(category_possible_sorted) // 5
+                number_category_in_last_row = len(category_possible_sorted) % 5
+                for start_index_category_by_int_row in \
+                        range(0, number_row_less_one * 5, 5):
+                    end_index_show_by_int_row = start_index_category_by_int_row\
+                                                + 4
+                    print('{0:<20}{1:<20}{2:<20}{3:<20}{4:<20}'
+                          .format(category_possible_sorted
+                                  [start_index_category_by_int_row],
+                                  category_possible_sorted
+                                  [start_index_category_by_int_row + 1],
+                                  category_possible_sorted
+                                  [start_index_category_by_int_row + 2],
+                                  category_possible_sorted
+                                  [start_index_category_by_int_row + 3],
+                                  category_possible_sorted
+                                  [end_index_show_by_int_row]))
+                if number_category_in_last_row == 1:
+                    print('{:<20}'
+                          .format(category_possible_sorted
+                                  [number_row_less_one * 5]))
+                elif number_category_in_last_row == 2:
+                    print('{0:<20}{1:<20}'
+                          .format(category_possible_sorted
+                                  [number_row_less_one * 5],
+                                  category_possible_sorted
+                                  [number_row_less_one * 5 + 1]))
+                elif number_category_in_last_row == 3:
+                    print('{0:<20}{1:<20}{2:<20}'
+                          .format(category_possible_sorted
+                                  [number_row_less_one * 5],
+                                  category_possible_sorted
+                                  [number_row_less_one * 5 + 1],
+                                  category_possible_sorted
+                                  [number_row_less_one * 5 + 2]))
+                elif number_category_in_last_row == 4:
+                    print('{0:<20}{1:<20}{2:<20}{3:<20}'
+                          .format(category_possible_sorted
+                                  [number_row_less_one * 5],
+                                  category_possible_sorted
+                                  [number_row_less_one * 5 + 1],
+                                  category_possible_sorted
+                                  [number_row_less_one * 5 + 2],
+                                  category_possible_sorted
+                                  [number_row_less_one * 5 + 3],))
+                all_input_user.clear()
+                all_input_user\
+                    .append(input(('\n' +
+                                   '{0:<16} : {1}\n' +
+                                   '{2:<16} : {3}\n' +
+                                   '{4:<16} : {5}\n')
+                                  .format('-noms catégories', 'toutes les '
+                                                              'entrées sur la '
+                                                              'même ligne',
+                                          '-all', 'TOUTES les catégories',
+                                          '-quit', 'interrompt le scrit')))
                 analyze_and_collect_input_user(all_input_user)
             elif input_user == '-all':
-                count_of_confirm_err = 0
-                while count_of_confirm_err == 0:
+                # Utilisation d'un compteur pour quitter aisément la boucle.
+                counter_of_confirm = 0
+                while counter_of_confirm == 0:
                     print('\nVous avez choisit TOUTES les catégories :')
-                    confirmation_select_category = input(('{0:<5} : {1}\n' +
-                                                         '{2:<5} : {3}\n')
-                                                         .format('y/n', 'confirmer/refaire la sélection',
-                                                                 '-quit', 'interrompt le script'))
+                    confirmation_select_category = \
+                        input(('{0:<5} : {1}\n' +
+                               '{2:<5} : {3}\n')
+                              .format('y/n', 'confirmer/refaire la sélection',
+                                      '-quit', 'interrompt le script'))
                     if confirmation_select_category == 'y':
                         for index_cat, cat in enumerate(category_possible):
-                            category_validate_to_scrap.append(category_possible[index_cat])
-                            count_of_confirm_err += 1
-                        # collect_url_books_by_one_category()
+                            category_validate_to_scrap\
+                                .append(category_possible[index_cat])
+                            counter_of_confirm += 1
                     elif confirmation_select_category == 'n':
-                        count_of_confirm_err += 1
+                        counter_of_confirm += 1
                         demand_of_user_what_category_to_scrap()
                     elif confirmation_select_category == '-quit':
                         sys.exit()
-                    elif confirmation_select_category != 'y' and confirmation_select_category != 'n':
+                    elif confirmation_select_category != 'y' \
+                            and confirmation_select_category != 'n':
                         print('\nRéponse invalide')
             elif input_user == '-quit':
                 sys.exit()
             elif input_user is not '-list' or input_user is not '-all':
                 analyze_category_select_by_user(all_input_user)
-
 
     def analyze_category_select_by_user(all_input_user):
         all_category_want_by_user = []
@@ -484,8 +545,9 @@ def selection_category_for_scrap(all_url_home_page_category_book):
 
     category_validate_temp = []
     category_validate_to_scrap = []
-    demand_of_user_what_category_to_scrap()
+    collect_category_possible(all_url_home_page_category_book)
     vers_la_suite_du_script(category_validate_to_scrap)
+
 
 def vers_la_suite_du_script(category_validate_to_scrap):
 
